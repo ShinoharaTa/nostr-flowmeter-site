a1abb38f23e354d98fd1495f0aa0bddd31c7a97d
tshinohara <tshinohara@spiritek.co.jp>
2023-08-08 14:56:41 +0900

    デザイン修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,12 @@
   <div class="fs-1">野州田川水系</div>
   <div class="fs-3 mt-2">定点観測所</div>
 </div>
-<div class="row max-width mx-auto pt-4 text-center">
+<div class="row max-width mx-auto pt-4 align-items-center">
   {#each relays as relay}
-    <div class="col-md-3 pt-3 fs-4">
+    <div class="col-md-6 fs-4 text-md-end text-center py-2">
       <a href="/{relay.key}" class="text-primary">{relay.river_name}</a>
     </div>
-    <div class="col-md-9 pt-2 fs-6">{relay.relay_url}</div>
+    <div class="col-md-6 fs-6 text-md-start text-center py-2">{relay.relay_url}</div>
   {/each}
 </div>
 <!-- {/if} -->
